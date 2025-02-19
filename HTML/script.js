4a336662ba1cdcb8ee141537780b5c6ef50ae2f3
ashutosh26l <aksmba2611@gmail.com>
2025-02-19 13:37:51 +0530

    first commit

---
+++ HTML/script.js
@@ -0,0 +1,5 @@
+function changeText() {
+    const messageElement = document.getElementById("message");
+    messageElement.textContent = "Hello, you just triggered a JavaScript function!";
+    messageElement.style.color = "blue";
+}
