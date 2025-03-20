90e522b35d9f4351c87abd3cd44ef12a3b9eb93b
ashutosh26l <aksmba2611@gmail.com>
2025-03-20 13:19:50 +0530

    basic js

---
+++ js/app.js
@@ -0,0 +1,10 @@
+a();
+function a(){
+    console.log("a");
+}
+
+const myName=true;
+console.log(typeof myName);
+const abc="ecec";
+const name="my name is :"+abc;
+console.log(name.toUpperCase());
