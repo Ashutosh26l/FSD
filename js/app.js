737f432a1ef8a2e2ee6cc75de7680c8c44da1366
ashutosh26l <aksmba2611@gmail.com>
2025-03-26 13:41:46 +0530

    .

--- js/app.js
+++ js/app.js
@@ -1,10 +1,19 @@
-a();
-function a(){
-    console.log("a");
-}
+// a();
+// function a(){
+//     console.log("a");
+// }
 
-const myName=true;
-console.log(typeof myName);
-const abc="ecec";
-const name="my name is :"+abc;
-console.log(name.toUpperCase());
+// const myName=true;
+// console.log(typeof myName);
+// const abc="ecec";
+// const name="my name is :"+abc;
+// console.log(name.toUpperCase());
+
+
+
+// var name;
+//name = "John";
+
+const products=["my products",120,true];
+products.push("new product");
+console.log(products);
